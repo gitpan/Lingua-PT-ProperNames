35dc181cf36d63f668e8c9ba9b7e78866746b103
Alberto Manuel Brandão Simões <ambs@perl-hackers.net>
2004-10-04 19:03:27 +0000

    Import of AMBS/Lingua-PT-ProperNames-0.01 from CPAN.
    gitpan-cpan-distribution: Lingua-PT-ProperNames
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         AMBS/Lingua/Lingua-PT-ProperNames-0.01.tar.gz
    gitpan-cpan-author:       AMBS
    gitpan-cpan-maturity:     released

---
+++ lib/Lingua/PT/ProperNames.pm
@@ -0,0 +1,412 @@
+package Lingua::PT::ProperNames;
+
+require Exporter;
+use locale;
+use warnings;
+use strict;
+
+=head1 NAME
+
+Lingua::PT::ProperNames - Simple module to extract proper names from Portuguese Text
+
+=head1 Version
+
+Version 0.01
+
+=cut
+
+
+our $VERSION = '0.01';
+our @ISA = qw(Exporter AutoLoader);
+our @EXPORT = qw/get print printString process forString/;
+
+our ($em, $np1, $np, $prof, $sep1, $sep2, %vazia, @stopw);
+
+BEGIN {
+
+  $np1 = qr{(?:(?:[A-ZÉÚÓÁÂ][.])+|[sS]r[.]|[dD]r[.]|St[oa]?[.]|[A-ZÉÚÓÁÂ]\w+(?:[\'\-]\w+)*)};
+
+  #if ($e) {
+  #$np= qr{$np1(?:\s+(?:d[eao]s?\s+|e\s+)?$np1)*};
+  #} else {
+  $np= qr{$np1(?:\s+(?:d[eao]s?\s+)?$np1)*};
+  #}
+
+  @stopw = qw{
+              no com se em segundo a o os as na nos nas do das dos da tanto
+              para de desde mas quando esta sem nem só apenas mesmo até uma uns um
+              pela por pelo pelas pelos depois ao sobre como umas já enquanto aos
+              também amanhã ontem embora essa nesse olhe hoje não eu ele eles
+              primeiro simplesmente era foi é será são seja nosso nossa nossos nossas
+              chama-se chamam-se subtitui resta diz salvo disse diz vamos entra entram
+              aqui começou lá seu vinham passou quanto sou vi onde este então temos
+              num aquele tivemos
+             };
+
+  $prof = join("|", qw{
+                       astrólogo astrónomo advogado actor
+                       baterista
+                       cantor compositor
+                       dramaturgo
+                       engenheiro escritor
+                       filósofo flautista físico
+                       investigador
+                       jogador
+                       matemático médico ministro músico
+                       pianista poeta professor
+                       químico
+                       teólogo
+                      });
+  $sep1 = join("|", qw{chamado "conhecido como"});
+
+  $sep2 = join("|", qw{brilhante conhecido reputado popular});
+  @vazia{@stopw} = (@stopw); # para ser mais facil ver se uma pal é stopword
+  $em = '\b(?:[Ee]m|[nN][oa]s?)';
+}
+
+=head1 Synopsis
+
+This module contains simple Perl-based functions to detect and extract
+proper names from Portuguese text.
+
+    use Lingua::PT::ProperNames;
+
+=head1 Export
+
+...
+
+=head1 Functions
+
+=head2 process
+
+=cut
+
+
+sub process{
+  ## opt:  in=> inputfile(sdtin), out => file(stdout)
+  my %opt = (sep => "", t => "normal" );
+
+  %opt = (%opt , %{shift(@_)}) if ref($_[0]) eq "HASH";
+
+  my $f=shift;
+  my $m="\x01";
+  my $f1;
+  my $old;
+  my $F1 ;
+
+  local $/ = $opt{sep};  # input record separator=1 or more empty lines
+
+  if (defined $opt{in}) {
+    open $F1, "$opt{in}" or die "cant open $opt{in}\n";
+  } else {
+    $F1=*STDIN;
+  }
+
+  if (defined $opt{out}) {
+    open F, ">$opt{out}" or die "cant create $opt{out}\n";
+    $old = select(F);
+  }
+
+  die "invalid parameter to 'forPN'" unless ref($f) eq "CODE";
+
+  if ($opt{t} eq "double") {
+    $f1 = shift;
+    die "invalid parameter ". ref($f1) unless ref($f1) eq "CODE";
+  }
+
+  while (<$F1>) {
+    my $ctx = $_;
+    if ($opt{t} eq "double") {
+
+      s{($np)}{$m($1$m)}g;
+      s{(^\s*|[-]\s+|[.!?]\s*)$m\(($np)$m\)}{
+	my ($aux1,$aux2,$aux3)= ($1,$2, &{$f1}($2,$ctx));
+	if   (defined($aux3)){$aux1 . $aux3}
+	else                 {$aux1 . _tryright($aux2)} }ge;
+      s{$m\(($np)$m\)}{   &{$f }($1,$ctx) }ge;
+
+    } else {
+      s{(\w+\s+|[\«\»,:()'`"]\s*)($np)}{$1 . &{$f }($2,$ctx) }ge;
+    }
+    print;
+  }
+  close $F1 if $opt{in};
+  if (defined $opt{out}) {
+    select $old;
+    close F;
+  }
+}
+
+=head2 forPNstring
+
+=cut
+
+sub forPNstring {
+  my $f = shift;
+  die "invalid parameter to 'forPNstring': function expected" unless ref($f) eq "CODE";
+  my $text = shift;
+  my $sep = shift || "\n";
+  my $r = '';
+  for (split(/$sep/,$text)) {
+    my $ctx = $_;
+    s/(\w+\s+|[\«\»,()'`i"]\s*)($np)/$1 . &{$f}($2,$ctx)/ge       ;
+    $r .= "$_$sep";
+  }
+  return $r;
+}
+
+=head2 printPNstring
+
+=cut
+
+sub printPNstring{
+  my $text = shift;
+  my %opt = ();
+
+  if   (ref($text) eq "HASH") { %opt = %$text        ; $text = shift; }
+  elsif(ref($text) eq "ARRAY"){ @opt{@$text} = @$text; $text = shift; }
+
+  my (%profissao, %names, %namesduv, %gnames);
+
+  for ($text) {
+    chop;
+    s/\n/ /g;
+    for (m/[.?!:;"]\s+($np1\s+$np)/gxs)  { $namesduv{$_}++ }
+    for (m![)>(]\s*($np1\s+$np)!gxs)     { $namesduv{$_}++ }
+    for (m/(?:[\w\«\»,]\s+)($np)/gxs)    { $names{$_}++ }
+    if ($opt{em}) {
+      for (/$em\s+($np)/g) { $gnames{$_}++ }
+    }
+    if ($opt{prof}) {
+      while(/\b($prof)\s+(?:(?:$sep1)\s+)?($np)/g)
+	{ $profissao{$2} = $1 }
+      while(/(?:[\w\«\»,]\s+|[(])($np),\s*(?:(?:$sep2)\s+)?($prof)/g)
+	{ $profissao{$1} = $2 }
+    }
+  }
+
+  # tratamento dos nomes "duvidosos" = Nome prop no inicio duma frase
+  #
+
+  for (keys %namesduv) {
+    if (/^(\w+)/ && $vazia{lc($1)}) { #exemplo "Como Jose Manuel"
+      s/^\w+\s*//;                    # retira-se a 1.a palavra
+      $names{$_}++
+    } else { 
+      $names{$_}++
+    }
+  }
+
+  for (keys %names) {
+    if (/^(\w+)/ && $vazia{lc($1)}) {  #exemplo "Como Jose Manuel"
+      my $ant = $_;
+      s/^\w+\s*//;                     # retira-se a 1.a palavra
+      $names{$_} += $names{$ant};
+      delete $names{$ant}
+    }
+  }
+
+  if ($opt{oco}) {
+    for (sort {$names{$b} <=> $names{$a}} keys %names ) {
+      printf("%60s - %d\n", $_ ,$names{$_});
+    }
+  } else {
+    if ($opt{comp}) {
+      my @l = sort _compara keys %names;
+      _compacta(\%names, @l)
+    } else {
+      for (sort _compara keys %names ) {
+	printf("%60s - %d\n", $_ ,$names{$_});
+      }
+    }
+    if ($opt{prof}) {
+      print "\nProfissões\n";
+      for (keys %profissao) {
+	print "$_ -- $profissao{$_}"
+      }
+    }
+    if ($opt{em}) {
+      print "\nGeograficos\n";
+      for (sort _compara keys %gnames ) {
+	printf("%60s - %d\n", $_ ,$gnames{$_})
+      }
+    }
+  }
+}
+
+
+=head2 getPN
+
+=cut
+
+sub getPN {
+  local $/ = "";           # input record separator=1 or more empty lines
+
+  my %opt;
+  @opt{@_} = @_;
+  my (%profissao, %names, %namesduv, %gnames);
+
+  while (<>) {
+    chop;
+    s/\n/ /g;
+    for (/[.?!:;"]\s+($np1\s+$np)/g)     { $namesduv{$_}++;}
+    for (/[)>(]\s*($np1\s+$np)/g)        { $namesduv{$_}++;}
+    for (/(?:[\w\«\»,]\s+)($np)/g)       { $names{$_}++;}
+    if ($opt{em}) {
+      for (/$em\s+($np)/g) { $gnames{$_}++;}}
+    if ($opt{prof}) {
+       while(/\b($prof)\s+(?:(?:$sep1)\s+)?($np)/g)
+	 { $profissao{$2} = $1 }
+       while(/(?:[\w\«\»,]\s+|[(])($np),\s*(?:(?:$sep2)\s+)?($prof)/g)
+	 { $profissao{$1} = $2 }
+     }
+  }
+
+  # tratamento dos nomes "duvidosos" = Nome prop no inicio duma frase
+  #
+
+  for (keys %namesduv) {
+    if(/^(\w+)/ && $vazia{lc($1)}) {  # exemplo "Como Jose Manuel"
+      s/^\w+\s*//;                    # retira-se a 1.a palavra
+      $names{$_}++
+    } else {
+      $names{$_}++
+    }
+  }
+  return (%names)
+}
+
+
+=head2 printPN
+
+=cut
+
+sub printPN{
+  local $/ = "";           # input record separator=1 or more empty lines
+
+  my %opt;
+  @opt{@_} = @_;
+  my (%profissao, %names, %namesduv, %gnames);
+
+  while (<>) {
+    chop;
+    s/\n/ /g;
+    for (/[.?!:;"]\s+($np1\s+$np)/g)     { $namesduv{$_}++ }
+    for (/[)>(]\s*($np1\s+$np)/g)        { $namesduv{$_}++ }
+    for (/(?:[\w\«\»,]\s+)($np)/g)       { $names{$_}++ }
+    if ($opt{em}) {
+      for (/$em\s+($np)/g) { $gnames{$_}++ }
+    }
+    if ($opt{prof}) {
+       while(/\b($prof)\s+(?:(?:$sep1)\s+)?($np)/g)
+	 { $profissao{$2} = $1 }
+       while(/(?:[\w\«\»,]\s+|[(])($np),\s*(?:(?:$sep2)\s+)?($prof)/g)
+	 { $profissao{$1} = $2 }
+     }
+  }
+
+  # tratamento dos nomes "duvidosos" = Nome prop no inicio duma frase
+  #
+
+  for (keys %namesduv){
+    if(/^(\w+)/ && $vazia{lc($1)} )   #exemplo "Como Jose Manuel"
+      {s/^\w+\s*//;                  # retira-se a 1.a palavra
+       $names{$_}++;}
+    else
+      { $names{$_}++;}
+  }
+
+  ##### Não sei bem se isto serve...
+
+  for (keys %names){
+    if(/^(\w+)/ && $vazia{lc($1)} )   #exemplo "Como Jose Manuel"
+      { my $ant = $_;
+        s/^\w+\s*//;                  # retira-se a 1.a palavra
+        $names{$_}+=$names{$ant};
+        delete $names{$ant};}
+  }
+
+  if($opt{oco}){
+    for (sort {$names{$b} <=> $names{$a}} keys %names )
+      {printf("%6d - %s\n",$names{$_}, $_ );}
+  }
+  else
+    {
+      if($opt{comp}){my @l = sort _compara keys %names;
+		     _compacta(\%names, @l); }
+      else{for (sort _compara keys %names )
+	     {printf("%60s - %d\n", $_ ,$names{$_});} }
+
+      if($opt{prof}){print "\nProfissões\n";
+		     for (keys %profissao){print "$_ -- $profissao{$_}";} }
+
+      if($opt{em}){print "\nGeograficos\n";
+		   for (sort _compara keys %gnames )
+		     {printf("%60s - %d\n", $_ ,$gnames{$_});} }
+  }
+}
+
+
+
+##
+# Auxiliary stuff
+
+sub _tryright{
+  my $a = shift;
+  return $a unless $a =~ /(\w+)/;
+  my $m = "\x01";
+  my ($w,$r) = ($1,$');
+  $r =~ s{($np)}{$m($1$m)}g;
+  return "$w$r";
+}
+
+
+sub _compacta{
+  my $s;
+  my $names = shift;
+
+  my $p = shift;
+  my $r = $p;
+  my $q = $names->{$p};
+  while ($s = shift)
+    { if ($s =~ (/^(.+) $p/)) { $r = "($1) $r" ;
+				$q += $names->{$s};
+			      }
+      else {print "$r - $q"; $r=$s; $q = $names->{$s}; }
+      $p=$s;
+    }
+  print "$r - $q";
+}
+
+sub _compara {
+  # ordena pela lista de palavras invertida
+  join(" ", reverse(split(" ",$a))) cmp join(" ", reverse(split(" ",$b)));
+}
+
+
+=head1 Author
+
+José João Almeida, C<< <jj@di.uminho.pt> >>
+
+Alberto Simões, C<< <ambs@di.uminho.pt> >>
+
+=head1 Bugs
+
+NOTE: We know documentation is inexistent. We are working on that for
+      very soon.
+
+Please report any bugs or feature requests to
+C<bug-lingua-pt-propernames@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.  I will be notified, and then you'll automatically
+be notified of progress on your bug as I make changes.
+
+=head1 Copyright & License
+
+Copyright 2004 José João Almeida, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1; # End of Lingua::PT::ProperNames
---
+++ t/01.forPN.t
@@ -0,0 +1,39 @@
+# -*- cperl -*-
+
+use Test::More tests => 5;
+
+BEGIN {
+  use_ok( 'Lingua::PT::ProperNames' );
+}
+
+use locale;
+use Data::Dumper;
+
+$a = 'à';
+
+SKIP: {
+  skip "not a good locale", 4 unless $a =~ m!^\w$!;
+
+  my $count=0;
+  my %pnlist=();
+  my $countD=0;
+  my %pnlistD=();
+
+  process({in=>"t/01.forPN.input"},
+	sub{$pnlist{n($_[0])}++; $count++});
+
+  is( $count, "322","forPN");
+  is( $pnlist{Portugal}, "5","forPN");
+  is( $pnlist{"Pimenta Machado"}, "4","forPN");
+  is( $pnlist{"Ribeiro da Silva"}, "1","forPN");
+
+  sub n{
+    my $a=shift;
+    for($a){s/\s+/ /g; s/^ //; s/ $//;}
+    $a;
+  }
+}
+1;
+
+
+
